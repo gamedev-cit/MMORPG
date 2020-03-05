1030b8cc85fad918f7bab7ef17b89a50a904379a
Shmel131 <ivan13shmel@gmail.com>
2020-03-05 19:24:48 +0300

    Следим за массивом всех кэректеров

--- src/Character.ts
+++ src/Character.ts
@@ -1,6 +1,22 @@
 import GameObject from "./GameObject";
+import GameStage from "./GameStage";
 
 export default class Character extends GameObject
 {
-	
+	onAdded()
+	{
+		super.onAdded()
+
+		GameStage.instance.characters.push(this)
+	}
+
+	onRemoved()
+	{
+		super.onRemoved()
+
+		let index = GameStage.instance.characters.indexOf(this)
+		if (index != -1) {
+			GameStage.instance.characters.splice(index, 1)
+		}
+	}
 }
--- src/GameStage.ts
+++ src/GameStage.ts
@@ -1,12 +1,15 @@
 import * as PIXI from 'pixi.js';
 import Stage from './Stage';
 import GameObject from './GameObject';
+import Character from './Character';
+
 
 export default class GameStage extends Stage
 {
-    static instance: GameStage
-    
-    gameObjects = new Array<GameObject>()
+	static instance: GameStage
+
+	gameObjects = new Array<GameObject>()
+	characters = new Array<Character>()
 
 	constructor()
 	{
