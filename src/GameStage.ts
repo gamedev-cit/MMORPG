be7ed242ca6dd2ed5acba6f8826f91a92db4ed39
DeathsSw0rd <61551625+DeathsSw0rd@users.noreply.github.com>
2020-03-12 19:13:07 +0300

    Общение с сервером

--- src/GameStage.ts
+++ src/GameStage.ts
@@ -1,4 +1,5 @@
 import * as PIXI from 'pixi.js';
+import * as io from 'socket.io-client';
 import Stage from './Stage';
 import GameObject from './GameObject';
 import Character from './Character';
@@ -11,12 +12,15 @@ export default class GameStage extends Stage
 {
 	static instance: GameStage
 
+	socket: SocketIOClient.Socket
+
 	gameObjects = new Array<GameObject>()
 	characters = new Array<Character>()
 
 	player: Player
-
 	playerController: PlayerController
+
+	debugLabel = new PIXI.Text()
 
 	constructor()
 	{
@@ -24,10 +28,38 @@ export default class GameStage extends Stage
 
 		GameStage.instance = this
 
-		this.player = new Knight()
+		this.socket = io.connect('wss://game-socket-server-1.appspot.com')
 
+		this.socket.on("state", (state: any) => this.onGameState(state))
+
+		this.player = new Knight()
 		this.playerController = new PlayerController(this.player)
 
-		this.addChild(this.player)	
+		this.addChild(this.player)
+
+		this.debugLabel.style.fill = 0xffffff
+		this.debugLabel.style.fontSize = 12
+		this.addChild(this.debugLabel)
+	}
+
+	onGameState(state: any)
+	{
+		this.debugLabel.text = JSON.stringify(state, null, "    ");
+	}
+
+	update()
+	{
+		this.socket.emit("character", {
+			"position": {
+				"x": this.player.x,
+				"y": this.player.y
+			},
+			"speed": {
+				"x": this.player.speedX,
+				"y": this.player.speedY
+			},
+			"radius": this.player.radius,
+			"type": "player"
+		})
 	}
 }
