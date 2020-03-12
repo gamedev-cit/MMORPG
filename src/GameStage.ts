f70f28f664d548d4e0b3d32d3e8be4b804ac6d49
Shmel131 <ivan13shmel@gmail.com>
2020-03-12 18:23:21 +0300

    Добавили игрока

--- src/GameStage.ts
+++ src/GameStage.ts
@@ -2,6 +2,8 @@ import * as PIXI from 'pixi.js';
 import Stage from './Stage';
 import GameObject from './GameObject';
 import Character from './Character';
+import Player from './Player';
+import Knight from './Knight';
 
 
 export default class GameStage extends Stage
@@ -11,10 +13,16 @@ export default class GameStage extends Stage
 	gameObjects = new Array<GameObject>()
 	characters = new Array<Character>()
 
+	player: Player
+
 	constructor()
 	{
 		super()
-		
+
 		GameStage.instance = this
+
+		this.player = new Knight()
+
+		this.addChild(this.player)	
 	}
 }
