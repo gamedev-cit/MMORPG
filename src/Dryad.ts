857bacaa2edced8bb44f275d9f71a497a7186f1d
SSS-G <61551688+SSS-G@users.noreply.github.com>
2020-03-12 19:34:25 +0300

    Create Dryad.ts

---
+++ src/Dryad.ts
@@ -0,0 +1,25 @@
+import Player from "./Player"
+import playerRunFrame1Url from './res/player_run_frame_1.png'
+import playerRunFrame2Url from './res/player_run_frame_2.png'
+
+
+export default class Dryad extends Player 
+{
+    sprite: PIXI.extras.AnimatedSprite
+
+	constructor()
+	{
+		super(40)
+
+		this.sprite = new PIXI.extras.AnimatedSprite([
+			PIXI.Texture.from(playerRunFrame1Url),
+			PIXI.Texture.from(playerRunFrame2Url)
+		])
+		this.sprite.anchor.x = 0.5
+		this.sprite.anchor.y = 0.5
+		this.addChild(this.sprite)
+
+		this.sprite.gotoAndPlay(0)
+		this.sprite.animationSpeed = 0.2
+	}
+}
