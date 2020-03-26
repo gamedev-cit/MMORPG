dfebcd955e32bbe1836c08235f1fb9674fecc3fe
DeathsSw0rd <61551625+DeathsSw0rd@users.noreply.github.com>
2020-03-26 18:32:18 +0300

    Анимация некроманта

--- src/Necromancer.ts
+++ src/Necromancer.ts
@@ -1,6 +1,7 @@
 import GameObject from "./GameObject";
 import Character from "./Character";
 import playerRunFrame1Url from './res/player_run_necromancer_frame_1.png'
+import playerRunFrame2Url from './res/player_run_necromancer_frame_2.png'
 
 import Player from "./Player";
 
@@ -14,13 +15,15 @@ export default class Necromancer extends Player
 		super(40)
 
 		this.sprite = new PIXI.extras.AnimatedSprite([
-			PIXI.Texture.from(playerRunFrame1Url)
+			PIXI.Texture.from(playerRunFrame1Url),
+			PIXI.Texture.from(playerRunFrame2Url)
+			
 		])
 		this.sprite.anchor.x = 0.5
 		this.sprite.anchor.y = 0.5
 		this.addChild(this.sprite)
 
 		this.sprite.gotoAndPlay(0)
-		this.sprite.animationSpeed = 0.2
+		this.sprite.animationSpeed = 0.05
 	}
 }
