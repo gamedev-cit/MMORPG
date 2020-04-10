e2bcca52944ec057684e0379fb2dbf72f1f82a0e
DeathsSw0rd <61551625+DeathsSw0rd@users.noreply.github.com>
2020-04-10 16:09:32 +0300

    Добавил анимации и рисунки

--- src/Knight.ts
+++ src/Knight.ts
@@ -1,4 +1,6 @@
 import playerRunFrame1Url from './res/player_run_knight_frame_1.png'
+import playerRunFrame2Url from './res/player_run_knight_frame_2.png'
+
 
 import Player from "./Player";
 
@@ -12,13 +14,14 @@ export default class Knight extends Player
 		super(40)
 
 		this.sprite = new PIXI.extras.AnimatedSprite([
-			PIXI.Texture.from(playerRunFrame1Url)
+			PIXI.Texture.from(playerRunFrame1Url),
+			PIXI.Texture.from(playerRunFrame2Url)
 		])
 		this.sprite.anchor.x = 0.5
 		this.sprite.anchor.y = 0.5
 		this.addChild(this.sprite)
 
 		this.sprite.gotoAndPlay(0)
-		this.sprite.animationSpeed = 0.2
+		this.sprite.animationSpeed = 0.075
 	}
 }
