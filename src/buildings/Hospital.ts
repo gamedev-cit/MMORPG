98faa00c63087972a22d1061dc3339b102e1860f
Pavel Alexeev <pasha.alexeev@gmail.com>
2020-04-20 19:37:39 +0300

    Располагаем зелья по кругу от больнички

--- src/buildings/Hospital.ts
+++ src/buildings/Hospital.ts
@@ -22,12 +22,14 @@ export default class Hospital extends GameObject
 			this.buildProgress += 1
 
 			if (this.buildProgress > 120) {
+				var angle = Math.random() * Math.PI*2
+				
 				GameStage.instance.socket.emit("item", {
 					"id": "item_" + Math.random(),
 					"type": "healkit",
 					"position": {
-						"x": this.x + 200,
-						"y": this.y,
+						"x": this.x + 200 * Math.cos(angle),
+						"y": this.y + 200 * Math.sin(angle),
 					}
 				})
 
