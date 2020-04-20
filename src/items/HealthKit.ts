76213f397889c38e16c282aac297b8c4def17347
Pavel Alexeev <pasha.alexeev@gmail.com>
2020-04-20 19:32:00 +0300

    Берём аптечки

--- src/items/HealthKit.ts
+++ src/items/HealthKit.ts
@@ -1,5 +1,7 @@
 import Item from "../Item";
 import itemUrl from '../res/health_pack.png'
+import GameObject from "../GameObject";
+import GameStage from "../GameStage";
 
 export default class HealthKit extends Item
 {
@@ -11,5 +13,17 @@ export default class HealthKit extends Item
 		sprite.anchor.x = 0.5
 		sprite.anchor.y = 0.5
 		this.addChild(sprite)
+	}
+
+	didHitGameObject(gameObject: GameObject)
+    {
+        if (gameObject == GameStage.instance.player) {
+			GameStage.instance.player.health += 30
+			GameStage.instance.world.removeChild(this)
+
+			GameStage.instance.socket.emit("delete_item", {
+				"id": this.id
+			})
+		}
 	}
 }
