0454f9c10aa1a67bf1dcb05cd54df19ec9841bff
Pavel Alexeev <pasha.alexeev@gmail.com>
2020-04-20 19:55:08 +0300

    Jotun (от Тимура)

--- src/Jotun.ts
+++ src/Jotun.ts
@@ -1,4 +1,5 @@
-import playerRunFrame1Url from './res/player_run_tree_4_frame.png'
+import playerRunFrame1Url from './res/player_run_jotun_frame_1.png'
+import playerRunFrame2Url from './res/player_run_jotun_frame_2.png'
 import Player from "./Player"; 
 
 export default class Jotun extends Player
@@ -11,13 +12,14 @@ export default class Jotun extends Player
 		super(40)
 
 		this.sprite = new PIXI.extras.AnimatedSprite([
-			PIXI.Texture.from(playerRunFrame1Url)
+			PIXI.Texture.from(playerRunFrame1Url),
+			PIXI.Texture.from(playerRunFrame2Url)
 		])
 		this.sprite.anchor.x = 0.5
 		this.sprite.anchor.y = 0.5
 		this.addChild(this.sprite)
 
 		this.sprite.gotoAndPlay(0)
-		this.sprite.animationSpeed = 0.2
+		this.sprite.animationSpeed = 0.05
 	}
 }
