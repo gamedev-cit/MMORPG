e688bd32c83cc99e5de91bccc0b4affc6a1246f2
DeathsSw0rd <61551625+DeathsSw0rd@users.noreply.github.com>
2020-04-27 16:01:03 +0300

    спрайты пчелы

--- src/Bee.ts
+++ src/Bee.ts
@@ -1,4 +1,7 @@
 import playerRunFrame1Url from './res/player_run_shmel_frame_1.png'
+import playerRunFrame2Url from './res/player_run_shmel_frame_2.png'
+import playerRunFrame3Url from './res/player_run_shmel_frame_3.png'
+import playerRunFrame4Url from './res/player_run_shmel_frame_4.png'
 
 import Player from "./Player";
 
@@ -12,7 +15,10 @@ export default class Bee extends Player
 		super(40)
 
 		this.sprite = new PIXI.extras.AnimatedSprite([
-			PIXI.Texture.from(playerRunFrame1Url)
+			PIXI.Texture.from(playerRunFrame1Url),
+			PIXI.Texture.from(playerRunFrame2Url),
+			PIXI.Texture.from(playerRunFrame3Url),
+			PIXI.Texture.from(playerRunFrame4Url)
 		])
 		this.sprite.anchor.x = 0.5
 		this.sprite.anchor.y = 0.5
